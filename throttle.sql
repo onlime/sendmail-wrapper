3e922cb45e49abab895f83d28d1115c2657c1df2
Philip Iezzi <github@onlime.ch>
2014-02-06 17:15:00 +0100

    added CREATE SCHEMA

--- throttle.sql
+++ throttle.sql
@@ -1,7 +1,15 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+
+CREATE SCHEMA IF NOT EXISTS `sendmailwrapper` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `sendmailwrapper`;
+
 -- -----------------------------------------------------
 -- Table `sendmail_throttle`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sendmail_throttle` (
+CREATE TABLE IF NOT EXISTS `throttle` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(30) NOT NULL DEFAULT '',
   `create_ts` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -14,3 +22,8 @@ CREATE TABLE IF NOT EXISTS `sendmail_throttle` (
   `status` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;

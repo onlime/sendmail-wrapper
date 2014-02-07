d2d7ad081c800b16a001ba2178e961cd8fe7eeb5
Philip Iezzi <github@onlime.ch>
2014-02-07 13:58:26 +0100

    minor fix

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ chown sendmailwrapper:sendmailwrapper sendmail-*
 chmod 755 sendmail-wrapper.php
 chmod 511 sendmail-throttle.php
 chmod 400 throttle.sql
+chattr +i sendmail-*.php
 
 ln -sf /opt/sendmail-wrapper/sendmail-wrapper.php /usr/sbin/sendmail-wrapper
 ln -sf /opt/sendmail-wrapper/sendmail-throttle.php /usr/sbin/sendmail-throttle

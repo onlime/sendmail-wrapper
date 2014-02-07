fd2bdf16db117fe5d477ce71970ec918de6dacb9
Philip Iezzi <github@onlime.ch>
2014-02-07 17:51:38 +0100

    fix for open_basedir restriction

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 cd /opt/sendmail-wrapper/
 
 chmod 700 .git
-chattr -i *.php
+chattr -i sendmail-*.php
 chown sendmailwrapper:sendmailwrapper *.php
 chmod 755 sendmail-wrapper.php prepend.php
 chmod 511 sendmail-throttle.php
 chmod 400 throttle.sql
-chattr +i *.php
+chattr +i sendmail-*.php
 
 ln -sf /opt/sendmail-wrapper/sendmail-wrapper.php /usr/sbin/sendmail-wrapper
 ln -sf /opt/sendmail-wrapper/sendmail-throttle.php /usr/sbin/sendmail-throttle
-ln -sf /opt/sendmail-wrapper/prepend.php /usr/sbin/php-prepend
+/bin/cp -a prepend.php /var/www/shared/
